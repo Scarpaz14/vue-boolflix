99dc46e22adfe6ea145fc827804b0d60b7ae901a
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-09 12:57:49 +0200

    creato header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-
+    <HeaderComponent/>
+    <MainComponent/>
   </div>
 </template>
 
 <script>
-
+import HeaderComponent from './components/blocks/HeaderComponent.vue';
+import MainComponent from './components/blocks/MainComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    HeaderComponent,
+    MainComponent,
   }
 }
 </script>
---
+++ src/components/blocks/HeaderComponent.vue
@@ -0,0 +1,39 @@
+<template>
+<header>
+    <div class="container-fluid d-flex align-items-center">
+        <img class="img-fluid" src="https://download.logo.wine/logo/Netflix/Netflix-Logo.wine.png" alt="">
+        <form>
+            <input class="search mx-3 py-1" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+
+
+</header>
+  
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+header{
+    background-color:#303030;
+    height: 80px;
+    display: flex;
+    align-items: center;
+}
+
+.container-fluid{
+    justify-content: space-between;
+}
+
+img{
+    width: 100px;
+}
+
+</style>
